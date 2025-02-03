c1374faf2b7506830ed1195799343fa6c16d5298
MHammadKhaan <itxmhkhan@gmail.com>
2025-02-03 13:21:45 +0500

    chnage in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    'no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
     semi: 'off',
   },
 };
